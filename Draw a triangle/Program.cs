d33547b80b14b70793e124b15dd47eafcd9915c5
Gamimmini <hnthanh922@gmail.com>
2023-05-27 17:31:20 +0700

    first commit

---
+++ Draw a triangle/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int limit = 5;        
+            int choice = -1;
+            while (choice != 0)
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Draw the rectangle");
+                Console.WriteLine("2. Draw the square top-left");
+                Console.WriteLine("3. Draw the square top-right");
+                Console.WriteLine("4. Draw the square botton-left");
+                Console.WriteLine("5. Draw the square botton-right");
+                Console.WriteLine("6. Draw isosceles triangle");
+                Console.WriteLine("7. Draw inverted isosceles triangle");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("Enter your choice: ");
+                choice = Int32.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        for (int i = 1; i <= 3; i++)
+                        {
+                            for (int j = 1; j <= 6; j++)
+                            {
+                                Console.Write("*");
+                            }
+                            Console.WriteLine(); 
+                        }
+                        break;
+                    case 2:
+                        for (int i = limit; i >= 1; i--)
+                        {
+                            for (int j = 1; j <= i; j++)
+                            {
+                                Console.Write("*");
+                            }
+                            Console.WriteLine(); 
+                        }
+                        break;
+                    case 3:
+                        for (int i = limit; i >= 1; i--)
+                        {
+                            for (int j = 1; j <= limit - i; j++)
+                            {
+                                Console.Write(" ");
+                            }
+                            for (int j = 1; j <= i; j++)
+                            {
+                                Console.Write("*");
+                            }
+                            Console.WriteLine(); 
+                        }
+                        break;
+                    case 4:
+                        for (int i = 1; i <= limit; i++)
+                        {
+                            for (int j = 1; j <= i; j++)
+                            {
+                                Console.Write("*");
+                            }
+                            Console.WriteLine(); 
+                        }
+                        break;
+                    case 5:
+                        for (int i = 1; i <= limit; i++)
+                        {
+                            for (int j = 1; j <= limit - i; j++)
+                            {
+                                Console.Write(" ");
+                            }
+                            for (int j = 1; j <= i; j++)
+                            {
+                                Console.Write("*");
+                            }
+                            Console.WriteLine(); 
+                        }
+                        break;
+                    case 6:
+                        for (int i = 1; i <= limit; i++)
+                        {
+                            for (int j = 1; j <= limit - i; j++)
+                            {
+                                Console.Write(" ");
+                            }
+                            for (int j = 1; j <= 2 * i - 1; j++)
+                            {
+                                Console.Write("*");
+                            }
+                            Console.WriteLine();
+                        }
+                        break;
+                    case 7:
+                        for (int i = limit; i >= 1; i--)
+                        {
+                            for (int j = 1; j <= limit - i; j++)
+                            {
+                                Console.Write(" ");
+                            }
+                            for (int j = 1; j <= 2 * i - 1; j++)
+                            {
+                                Console.Write("*");
+                            }
+                            Console.WriteLine(); 
+                        }
+                        break;     
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("No choice!");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ read numbers into words/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Enter a non-negative integer between 0 and 999:");
+            int number = int.Parse(Console.ReadLine());
+            string result = ReadNumber(number);
+            Console.WriteLine("The number {0} is read as: {1}", number, result);
+        }
+        static string ReadNumber(int number)
+        {
+            if (number == 0)
+            {
+                return "zero";
+            }
+            else if (number < 0 || number > 999)
+            {
+                return "Out of ability";
+            }
+            
+            string[] units = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            string[] tens = { "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+            string[] teens = { "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+
+            string result = "";
+
+            int hundredsDigit = number / 100;
+            int tensDigit = (number % 100) / 10;
+            int unitsDigit = number % 10;
+
+            if (hundredsDigit > 0)
+            {
+                result += units[hundredsDigit] + " hundred";
+                if (tensDigit > 0 || unitsDigit > 0)
+                {
+                    result += " and";
+                }
+            }
+
+            if (tensDigit > 1)
+            {
+                result += " " + tens[tensDigit];
+                if (unitsDigit > 0)
+                {
+                    result += " " + units[unitsDigit];
+                }
+            }
+            else if (tensDigit == 1)
+            {
+                result += " " + teens[unitsDigit];
+            }
+            else
+            {
+                result += " " + units[unitsDigit];
+            }
+
+            return result;
+            }
+    }
+}
